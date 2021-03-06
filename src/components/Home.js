47a47802db4eb6e1f26ed40a84a53d78dbbde5b9
mmarkham128 <mmarkham128@gmail.com>
2021-03-05 20:14:58 -0600

    fixing2

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ export default function Home(){
     return (
         <main className="h-100px">
             <img src={image} alt="Background" className="absolute object-cover w-screen h-full" />
-            <section className="relative flex justify-center min-h-screen pt-0 lg:pt-5 px-8">
+            <section className="relative flex justify-center min-h-screen pt-0 lg:pt-3 px-8">
                 <h1 className="text-6xl text-yellow-600 font-bold cursive leading-none lg:leading-snug home-name">Michael Markham<br/><br/><br/> Software Developer</h1>
             </section>
         </main>
