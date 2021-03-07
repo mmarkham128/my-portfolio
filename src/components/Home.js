bdba3b13f94132e2bdb632072457a1147d97d176
mmarkham128 <mmarkham128@gmail.com>
2021-03-06 18:35:32 -0600

    fixing some more2

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,8 +5,13 @@ export default function Home(){
     return (
         <main className="h-100px">
             <img src={image} alt="Background" className="absolute object-cover w-screen h-full" />
-            <section className="relative flex justify-center min-h-screen pt-0 lg:pt-10 px-8">
-                <h1 className="text-6xl text-yellow-600 font-bold cursive leading-none lg:leading-snug home-name">Michael Markham<br/><br/><br/> Software Developer</h1>
+            <section className="space-y-96 lg:space-y-92 sm:space-y-72 md:space-y-96 xl:space-y-64">
+            <div className=" relative flex justify-center pt-28 sm:pt-24 xl:pt-12 lg:pt-48 md:pt-36 px-8">
+                <h1 className=" text-3xl lg:text-7xl sm:text-7xl text-yellow-600 font-bold cursive leading-none lg:leading-snug ">Michael Markham</h1>
+            </div>
+            <div className=" relative flex justify-center pt-4 xl:pt-16 lg:pt-40 md:pt-8 px-8">
+                <h2 className=" text-3xl lg:text-7xl sm:text-7xl text-yellow-600 font-bold cursive leading-none lg:leading-snug ">Software Developer</h2>
+            </div>
             </section>
         </main>
     )
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,32 +10,32 @@ export default function NavBar(){
                 <nav className="flex">
                     <NavLink to="/" exact
                     activeClassName="text-white" 
-                    className="inline-flex items-center py-0 px-2 mr-4 text-white hover:text-yellow-600 text-4xl font-bold cursive tracking-widest">
+                    className=" text-sm sm:text-6xl inline-flex items-center sm:py-0 sm:px-2 sm:mr-4 mr-4 text-white hover:text-yellow-600 font-bold cursive tracking-widest">
                         Michael
                     </NavLink>
                     <NavLink to="/post"
                     activeClassName="text-yellow-600 bg-white"
-                    className= "inline-flex items-center py-0 px-3 my-6 rounded text-white hover:text-yellow-600"
+                    className= "text-xs  sm:text-2xl inline-flex items-center sm:py-0 sm:px-3 sm:my-6 mr-2 rounded text-white hover:text-yellow-600"
                     >
                         Blog Posts
                     </NavLink >
                     <NavLink to="/project"
                     activeClassName="text-yellow-600 bg-white"
-                    className= "inline-flex items-center py-0 px-3 my-6 rounded text-white hover:text-yellow-600"
+                    className= "text-xs sm:text-2xl inline-flex items-center sm:py-0 sm:px-3 sm:my-6 mr-2 rounded text-white hover:text-yellow-600"
                     >
                         Projects
                     </NavLink>
                     <NavLink to="/about"
                     activeClassName="text-yellow-600 bg-white"
-                    className= "inline-flex items-center py-0 px-3 my-6 rounded text-white hover:text-yellow-600"
+                    className= "text-xs sm:text-2xl inline-flex items-center sm:py-0 sm:px-3 sm:my-6 mr-2 rounded text-white hover:text-yellow-600"
                     >
                         About Me!
                     </NavLink>
                 </nav>
-                <div className="inline-flex py-0 px-3 my-6">
-                    <SocialIcon url="https://github.com/mmarkham128" className="mr-4" target="_blank" fgColor="#fff" style={{ height:35, width: 35}} />
-                    <SocialIcon url="https://www.facebook.com/ryan.markham.334/" className="mr-4" target="_blank" fgColor="#fff" style={{ height:35, width: 35}} />
-                    <SocialIcon url="https://www.linkedin.com/in/mmarkham128/" className="mr-4" target="_blank" fgColor="#fff" style={{ height:35, width: 35}} /> 
+                <div className="inline-flex  sm:py-0 sm:px-3 sm:my-6">
+                    <SocialIcon url="https://github.com/mmarkham128" className="sm:mr-4" target="_blank" fgColor="#fff" style={{ height:30, width: 30}} />
+                    <SocialIcon url="https://www.facebook.com/ryan.markham.334/" className="sm:mr-4" target="_blank" fgColor="#fff" style={{ height:30, width: 30}} />
+                    <SocialIcon url="https://www.linkedin.com/in/mmarkham128/" className="sm:mr-4" target="_blank" fgColor="#fff" style={{ height:30, width: 30}} /> 
 
                 </div>
             </div>
