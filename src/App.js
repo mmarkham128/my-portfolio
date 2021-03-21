de835ccda6f87acd6b13e6f2dbdf69beffaaa714
mmarkham128 <mmarkham128@gmail.com>
2021-03-21 01:15:28 -0500

    aaaannnddd more stuff2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import {BrowserRouter, Route, Switch} from "react-router-dom";
-import Home from "./components/Home";
 import About from "./components/About";
 import  Post  from "./components/Post";
 import  Project  from "./components/Project";
